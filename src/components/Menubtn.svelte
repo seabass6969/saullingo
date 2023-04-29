80c975c7d139421a4916d90c24abc2ef8fc253e2
BuildTools <cephas8080@outlook.com>
2023-04-29 20:06:57 +0100

    more style change

---
+++ src/components/Menubtn.svelte
@@ -0,0 +1,24 @@
+<button class="stats" on:click><span class="material-symbols-outlined">menu</span> </button>
+<style lang="scss">
+	.stats{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 35px;
+		height: 35px;
+		border-radius: 10px;	
+		// margin-top: 3.5vw;
+		// margin-bottom: 2vw;
+		margin-left: 1vw;
+		background-color: $friendly-color;
+		border: 0px;
+		@include boxshadow-btn-small;
+		@media (min-width: 810px){
+            width: 45px;
+            height: 45px;
+            // margin-bottom: 3vw;
+            // margin-top: 2vw;
+            margin-right: 1vw;
+		}
+	}
+</style>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,74 @@
+<script>
+	import { slide } from "svelte/transition";
+	import { OpenDialog } from "../lib/DialogUtils";
+	import { pageType } from "../lib/Question";
+	import { navbarOpen, page } from "../lib/stores";
+	import Close from "./Close.svelte";
+	import Settingbtn from "./Settingbtn.svelte";
+	import Statisticbtn from "./Statisticbtn.svelte";
+
+	const settingsPage = () => {
+        navbarOpen.set(false)
+		page.set(pageType.settings)
+	}
+    const closes = () => {
+        navbarOpen.set(false)
+    }
+    const statisticPage = () => {
+        navbarOpen.set(false)
+        OpenDialog("Is just not there yet🚧")
+    }
+    let clientWidth = 0
+</script>
+<svelte:window bind:innerWidth={clientWidth} />
+<div class="leftnav" in:slide={{duration: 1000}} out:slide={{duration: 1000}}>
+    {#if clientWidth > 810}
+        <div class="topset">
+            <img src="/favicon.svg" alt="" class="logo">
+            <div>
+            <span class="title">Saullingo</span>
+            </div>
+        </div>
+    {/if}
+    {#if clientWidth < 810}
+        <Close on:click={closes}/>
+    {/if}
+    <Settingbtn on:click={settingsPage}/>
+    <Statisticbtn on:click={statisticPage}/>
+</div>
+<style lang="scss">
+    .leftnav {
+        z-index: 999;
+        height: 100vh;
+        background-color: $base-color;
+        backdrop-filter: blur(5px);
+		border-radius: 0px 35px 35px 0px;
+        display: grid;
+        grid-template-rows: repeat(3, 12vh);
+        align-items: center;
+        padding-left: 2vw;
+        padding-right: 4vw;
+        width: 36vw;
+        position: fixed;
+		@media (min-width: 810px){
+            width: 15vw;
+            position: relative;
+            min-width: 15vw;
+        }
+    }
+.topset {
+	display: grid;
+	align-items: center;
+	justify-items: left;
+	height: 13vh;
+    grid-template-columns: 5vw 10vw ;
+}
+.logo {
+	width: 10vw;
+	width: 5vw;
+}
+	.title {
+        margin-left: 1vw;
+		@include titlefont;
+	}
+</style>        
